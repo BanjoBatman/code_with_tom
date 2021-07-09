50d8c0a9160e0b9453b2530e7cd6d5d95f220e0a
Tomas <elke0016@gmail.com>
2021-07-09 08:48:47 -0500

    Structs, what are those?

---
+++ struct.ino
@@ -0,0 +1,69 @@
+template <class anyClass> void swapVals(anyClass &a,anyClass &b){
+  anyClass temp;
+  temp = a;
+  a=b;
+  b=temp;
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  int a = 5;
+  int b = 6;
+  double da = 5.0;
+  double db = 6.0;
+  String sa = "hey";
+  String sb = "there";
+  
+  Serial.begin(9600);
+  Serial.print("a=");
+  Serial.println(a);
+  Serial.print("b=");
+  Serial.println(b);
+  Serial.print("da=");
+  Serial.println(da);
+  Serial.print("db=");
+  Serial.println(db);
+  Serial.print("sa=");
+  Serial.println(sa);
+  Serial.print("sb=");
+  Serial.println(sb);
+  // prints 
+  //a=5
+  //b=6
+  //da=5.0
+  //db=6.0
+  //sa="hey"
+  //sb="there"
+  swapVals(a,b);// swaps a and b
+  swapVals(da,db);// swaps da and db
+  swapVals(sa,sb);// swaps sa and sb
+ 
+  Serial.print("a=");
+  Serial.println(a);
+  Serial.print("b=");
+  Serial.println(b);
+  Serial.print("da=");
+  Serial.println(da);
+  Serial.print("db=");
+  Serial.println(db);
+  Serial.print("sa=");
+  Serial.println(sa);
+  Serial.print("sb=");
+  Serial.println(sb);
+  // prints 
+  //a=6
+  //b=5
+  //da=6.0
+  //db=5.0
+  //sa="there"
+  //sb="hey"
+
+  swapVals(a, sb);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
